b0fc55f706efacf52c3dc2e40926732ed3a473c1
Alan Chan <github@zenan.ch>
2017-08-06 14:07:10 +0800

    Add mobile site support and Travis caching

--- motion.py
+++ motion.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-
 from os.path import exists
 from os import mkdir
 import requests
@@ -10,14 +9,32 @@ import time
 from const import assets
 from os import environ as options
 
-driver = webdriver.Chrome()
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
 
-visited = set()
 blacklist = ["inter", "segment", "facebook", "fullstory", "loggly.js", "app-"]
 notions = {}
+user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A300 Safari/602.1'
+visited = set()
+notions = {}
+
+
+def motion(is_mobile=False):
+    global visited, notions
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    if is_mobile:
+        chrome_options.add_argument('--user-agent=' + user_agent)
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    visited = set()
+    notions = {}
+    print("Parsing index...")
+    n = Notion(options['index'], driver, options=options, is_index=True)
+    n.mod()
+    print("Index page looks good.")
+    n.walk()
+    n.save()
+    print("Site generated successfully.")
+    driver.quit()
 
 
 def download_file(url, local_filename):
@@ -51,11 +68,13 @@ def md(local_filename):
 
 
 class Notion:
-    def __init__(self, url, options=None, is_index=False, wait=0):
+    def __init__(self, url, driver, options=None, is_index=False, wait=0, is_mobile=False):
+        self.driver = driver
         print("Visiting " + url)
         self.is_index = is_index
+        self.is_mobile = is_mobile
         self.url = url
-        driver.get(url)
+        self.driver.get(url)
         time.sleep(wait)
         self.dom = BeautifulSoup(driver.page_source, "html.parser")
         self.links = set()
@@ -76,21 +95,24 @@ class Notion:
     def mod(self, no_retry=False):
         try:
             self.meta()
+            self.remove_overlay()
             self.parse_links()
             self.remove_scripts()
             self.save_assets()
         except:
             time.sleep(2)
-            print("Exception occurred, retrying...")
-            self.dom = BeautifulSoup(driver.page_source, "html.parser")
+            print("Exception occurred, sleep for 2 secs and retry...")
+            self.dom = BeautifulSoup(self.driver.page_source, "html.parser")
             if no_retry:
                 raise
             else:
                 self.mod(no_retry=True)
 
-
     def save(self):
-        local_filename = "site/" + self.filename
+        if self.is_mobile:
+            local_filename = "site/m/" + self.filename
+        else:
+            local_filename = "site/" + self.filename
         with open(local_filename, "w") as f:
             f.write(str(self.dom))
 
@@ -98,7 +120,7 @@ class Notion:
         for a in self.dom.find_all("a"):
             href = a['href']
             if href.startswith('/'):
-                if href == "/login":
+                if href == '/login':
                     a.decompose()
                 elif href[1:] == self.options["index"].split("/")[-1]:
                     a['href'] = '/'
@@ -121,32 +143,42 @@ class Notion:
             self.title = title + ' ' + \
                 self.options["title_sep"] + ' ' + self.options["site_title"]
         self.dom.find("title").string = self.title
-        self.dom.find("meta", attrs={"name": "twitter:site"})["content"] = self.options["twitter"]
+        self.dom.find("meta", attrs={"name": "twitter:site"})[
+            "content"] = self.options["twitter"]
         page_path = '-'.join(self.url.split("/")[-1].split('-')[:-1])
-        self.dom.find("meta", attrs={"name": "twitter:url"})["content"] = self.options["base_url"] + page_path
-        self.dom.find("meta", attrs={"property": "og:url"})["content"] = self.options["base_url"] + page_path
-        self.dom.find("meta", attrs={"property": "og:title"})["content"] = self.title
-        self.dom.find("meta", attrs={"name": "twitter:title"})["content"] = self.title
-        self.dom.find("meta", attrs={"property": "og:site_name"})["content"] = self.options["site_title"]
-        self.dom.find("meta", attrs={"name": "description"})["content"] = self.options["description"]
-        self.dom.find("meta", attrs={"name": "twitter:description"})["content"] = self.options["description"]
-        self.dom.find("meta", attrs={"property": "og:description"})["content"] = self.options["description"]
+        self.dom.find("meta", attrs={"name": "twitter:url"})[
+            "content"] = self.options["base_url"] + page_path
+        self.dom.find("meta", attrs={"property": "og:url"})[
+            "content"] = self.options["base_url"] + page_path
+        self.dom.find("meta", attrs={"property": "og:title"})[
+            "content"] = self.title
+        self.dom.find("meta", attrs={"name": "twitter:title"})[
+            "content"] = self.title
+        self.dom.find("meta", attrs={"property": "og:site_name"})[
+            "content"] = self.options["site_title"]
+        self.dom.find("meta", attrs={"name": "description"})[
+            "content"] = self.options["description"]
+        self.dom.find("meta", attrs={"name": "twitter:description"})[
+            "content"] = self.options["description"]
+        self.dom.find("meta", attrs={"property": "og:description"})[
+            "content"] = self.options["description"]
         print("Title: " + self.dom.find("title").string)
-        imgs = [i for i in self.dom.find_all('img') if i.has_attr("style") and "30vh" in i["style"]]
+        imgs = [i for i in self.dom.find_all('img') if i.has_attr(
+            "style") and "30vh" in i["style"]]
         if imgs:
             img_url = self.options["base_url"] + imgs[0]["src"][1:]
-            self.dom.find("meta", attrs={"property": "og:image"})["content"] = img_url
-            self.dom.find("meta", attrs={"name": "twitter:image"})["content"] = img_url
+            self.dom.find("meta", attrs={"property": "og:image"})[
+                "content"] = img_url
+            self.dom.find("meta", attrs={"name": "twitter:image"})[
+                "content"] = img_url
         else:
             self.dom.find("meta", attrs={"property": "og:image"}).decompose()
             self.dom.find("meta", attrs={"name": "twitter:image"}).decompose()
-
 
     def remove_scripts(self):
         for s in self.dom.find_all("script"):
             if s.has_attr("src"):
                 if any([bool(b in s["src"]) for b in blacklist]):
-                    #                     print(s)
                     pass
                 else:
                     continue
@@ -169,10 +201,16 @@ class Notion:
                 download_file("https://notion.so" +
                               script["src"], script["src"][1:])
 
+    def remove_overlay(self):
+        overlay = self.dom.find(class_="notion-overlay-container")
+        if overlay:
+            overlay.decompose()
+
     def walk(self):
+        global visited
         for link in self.links:
             if link not in visited:
-                page = Notion("https://notion.so" + link, options=options)
+                page = Notion("https://notion.so" + link, self.driver, options=options, is_mobile=self.is_mobile)
                 notions[link] = page
                 page.mod()
                 page.save()
@@ -181,12 +219,5 @@ class Notion:
 
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    print("Parsing index...")
-    n = Notion(options['index'], options, is_index=True)
-    n.mod()
-    print("Index page looks good.")
-    n.walk()
-    n.save()
-    print("Site generated successfully.")
-    driver.quit()
+    motion()
+    motion(is_mobile=True)
