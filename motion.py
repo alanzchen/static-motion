07dc65f87592157b3e835f2f0dd591083bf9b163
Alan Chan <github@zenan.ch>
2017-08-25 12:14:15 +0800

    bug fix

--- motion.py
+++ motion.py
@@ -196,7 +196,7 @@ class Notion:
                 in_html = False
                 div.decompose()
             if in_html:
-                div.string = div.text
+                div.replace_with(BeautifulSoup(div.text), "html.parser")
                 continue
             # For lightGallery.js
             img = div.find('img')
