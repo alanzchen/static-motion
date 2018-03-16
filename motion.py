c7fb837a45fdef4927b39113e5341a108bbf1c30
Alan Chan <github@zenan.ch>
2018-03-16 17:04:09 +0800

    Change the title detection method

--- motion.py
+++ motion.py
@@ -283,12 +283,8 @@ class Notion:
     def meta(self):
         if self.dom.find('html').has_attr("manifest"):
             self.dom.find('html')["manifest"] = ''
-        if not self.is_mobile:
-            titles = [i for i in self.dom.find_all(
-                "div") if (i.has_attr("style") and i.has_attr('data-block-id') and "2.25em" in i["style"])]
-        else:
-            titles = [i for i in self.dom.find_all(
-                "div") if (i.has_attr("style") and i.has_attr('data-block-id') and "2em" in i["style"])]
+        titles = [i for i in self.dom.find_all(
+            "div") if (i.has_attr("style") and i["placeholder"] == 'Untitled')]
         title = titles[0].text.strip()
         titles[0]["id"] = 'title'
         if self.is_index:
